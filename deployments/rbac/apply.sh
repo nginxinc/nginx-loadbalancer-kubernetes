cf42609197f18ee93f97a10db8e111550dfe6640
Otter Prime Sr (steve) <s.wagner@f5.com>
2023-06-30 12:54:21 -0700

    Fix Cluster Role, pathing in script, and README instructions

--- deployments/rbac/apply.sh
+++ deployments/rbac/apply.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+pushd "$(dirname "$0")"
+
 echo "Applying all RBAC resources..."
 
 kubectl apply -f serviceaccount.yaml
 kubectl apply -f clusterrole.yaml
 kubectl apply -f clusterrolebinding.yaml
 kubectl apply -f secret.yaml
+
+popd
