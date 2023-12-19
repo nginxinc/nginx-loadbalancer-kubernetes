1eac96915eb725bed785fd6170126c97dd0dab4a
Steve Wagner <ciroque@live.com>
2023-12-18 16:55:58 -0800

    Better test coverage (#132)
    * Better test coverage across most modules
    
    - improve RoundTripper coverage
    - exclude coverage files from source control
    - Test coverage for UpstreamServer
    - Test coverage for probation server module
    - Test coverage for Event module
    - ignore test coverage files
    
    * - address PR feedback

---
+++ internal/core/upstream_server_test.go
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023 F5 Inc. All rights reserved.
+ * Use of this source code is governed by the Apache License that can be found in the LICENSE file.
+ */
+
+package core
+
+import "testing"
+
+func TestNewUpstreamServer(t *testing.T) {
+	host := "localhost"
+	us := NewUpstreamServer(host)
+	if us.Host != host {
+		t.Errorf("NewUpstreamServer(%s) = %s; want %s", host, us.Host, host)
+	}
+}
