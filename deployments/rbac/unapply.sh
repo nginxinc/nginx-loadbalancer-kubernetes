d4a4eb0ffbedf1e9b329444473486369118fa3b8
sarna <P.Sarna@f5.com>
2024-10-06 22:57:51 -0700

    Remove unused YAMLs
    We have officially transitions towards using Helm
    to install NLK. With that, we should also look at
    auto-generating these YAMLs via helm and not
    hardcode them into the repository.
    
    Removing them as they also add additional grep
    confusion while scanning through the repo.

--- deployments/rbac/unapply.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Unapplying all RBAC resources..."
-
-kubectl delete -f serviceaccount.yaml
-kubectl delete -f clusterrole.yaml
-kubectl delete -f clusterrolebinding.yaml
-kubectl delete -f secret.yaml
