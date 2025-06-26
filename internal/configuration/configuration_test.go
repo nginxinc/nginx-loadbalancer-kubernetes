2197336b5f258be99f161f1d70d525d5f94e1e7b
Alex Russell <a.russell@f5.com>
2025-06-26 15:32:04 -0600

    Fixed up helm templating for nginx-hosts param to support multiple nginx hosts
    In order for the nginx-hosts yaml field to be parsed correctly by viper the template needs to:
    
    1. not put double quotes around the value (this causes viper to interpret it as a string)
    2. render it as a JSON array rather than a go representation of a slice.

--- internal/configuration/configuration_test.go
+++ internal/configuration/configuration_test.go
@@ -12,41 +12,91 @@ import (
 
 func TestConfiguration(t *testing.T) {
 	t.Parallel()
-	expectedSettings := configuration.Settings{
-		LogLevel:       "warn",
-		NginxPlusHosts: []string{"https://10.0.0.1:9000/api"},
-		TLSMode:        configuration.NoTLS,
-		Certificates: &certification.Certificates{
-			CaCertificateSecretKey:     "fakeCAKey",
-			ClientCertificateSecretKey: "fakeCertKey",
-		},
-		Handler: configuration.HandlerSettings{
-			RetryCount: 5,
-			Threads:    1,
-			WorkQueueSettings: configuration.WorkQueueSettings{
-				RateLimiterBase: time.Second * 2,
-				RateLimiterMax:  time.Second * 60,
-				Name:            "nlk-handler",
+
+	tests := map[string]struct {
+		testFile         string
+		expectedSettings configuration.Settings
+	}{
+		"one nginx plus host": {
+			testFile: "one-nginx-host",
+			expectedSettings: configuration.Settings{
+				LogLevel:       "warn",
+				NginxPlusHosts: []string{"https://10.0.0.1:9000/api"},
+				TLSMode:        configuration.NoTLS,
+				Certificates: &certification.Certificates{
+					CaCertificateSecretKey:     "fakeCAKey",
+					ClientCertificateSecretKey: "fakeCertKey",
+				},
+				Handler: configuration.HandlerSettings{
+					RetryCount: 5,
+					Threads:    1,
+					WorkQueueSettings: configuration.WorkQueueSettings{
+						RateLimiterBase: time.Second * 2,
+						RateLimiterMax:  time.Second * 60,
+						Name:            "nlk-handler",
+					},
+				},
+				Synchronizer: configuration.SynchronizerSettings{
+					MaxMillisecondsJitter: 750,
+					MinMillisecondsJitter: 250,
+					RetryCount:            5,
+					Threads:               1,
+					WorkQueueSettings: configuration.WorkQueueSettings{
+						RateLimiterBase: time.Second * 2,
+						RateLimiterMax:  time.Second * 60,
+						Name:            "nlk-synchronizer",
+					},
+				},
+				Watcher: configuration.WatcherSettings{
+					ResyncPeriod:      0,
+					ServiceAnnotation: "fakeServiceMatch",
+				},
 			},
 		},
-		Synchronizer: configuration.SynchronizerSettings{
-			MaxMillisecondsJitter: 750,
-			MinMillisecondsJitter: 250,
-			RetryCount:            5,
-			Threads:               1,
-			WorkQueueSettings: configuration.WorkQueueSettings{
-				RateLimiterBase: time.Second * 2,
-				RateLimiterMax:  time.Second * 60,
-				Name:            "nlk-synchronizer",
+		"multiple nginx plus hosts": {
+			testFile: "multiple-nginx-hosts",
+			expectedSettings: configuration.Settings{
+				LogLevel:       "warn",
+				NginxPlusHosts: []string{"https://10.0.0.1:9000/api", "https://10.0.0.2:9000/api"},
+				TLSMode:        configuration.NoTLS,
+				Certificates: &certification.Certificates{
+					CaCertificateSecretKey:     "fakeCAKey",
+					ClientCertificateSecretKey: "fakeCertKey",
+				},
+				Handler: configuration.HandlerSettings{
+					RetryCount: 5,
+					Threads:    1,
+					WorkQueueSettings: configuration.WorkQueueSettings{
+						RateLimiterBase: time.Second * 2,
+						RateLimiterMax:  time.Second * 60,
+						Name:            "nlk-handler",
+					},
+				},
+				Synchronizer: configuration.SynchronizerSettings{
+					MaxMillisecondsJitter: 750,
+					MinMillisecondsJitter: 250,
+					RetryCount:            5,
+					Threads:               1,
+					WorkQueueSettings: configuration.WorkQueueSettings{
+						RateLimiterBase: time.Second * 2,
+						RateLimiterMax:  time.Second * 60,
+						Name:            "nlk-synchronizer",
+					},
+				},
+				Watcher: configuration.WatcherSettings{
+					ResyncPeriod:      0,
+					ServiceAnnotation: "fakeServiceMatch",
+				},
 			},
-		},
-		Watcher: configuration.WatcherSettings{
-			ResyncPeriod:      0,
-			ServiceAnnotation: "fakeServiceMatch",
 		},
 	}
 
-	settings, err := configuration.Read("test", "./testdata")
-	require.NoError(t, err)
-	require.Equal(t, expectedSettings, settings)
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			settings, err := configuration.Read(tc.testFile, "./testdata")
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedSettings, settings)
+		})
+	}
 }
